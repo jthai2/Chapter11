3921ba893990e989300f945561928e161ce64834
jthai2 <jimmythai35@gmail.com>
2018-04-27 13:20:31 -0500

    update a lot

---
+++ Intermediate23/Intermediate23/Source.cpp
@@ -0,0 +1,21 @@
+//Intermediate23.cpp - displays the contents of two parallel arrays
+//4/27/18
+//Jimmy Thai 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int points[5] = { 62, 75, 100, 83, 85 };
+	char grades[5] = { 'D', 'C', 'A', 'B', 'B' };
+
+	int x = 0;
+
+	cout << points[x] << "->" << grades[x] << endl;
+	x += 1;
+
+
+	system("pause");
+	return 0;
+}	//end of main function
